878d7c036249338a0bbb927e385b5229b785633c
Caitlin <caitlinbrakel@outlook.com>
2021-02-02 16:18:10 -0600

    Created production company and movie models.  Seeded the db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Movie.delete_all
+ProductionCompany.delete_all
+
+# Rails.root... ./db/top_movies.csv
+filename = Rails.root.join("db/top_movies.csv")
+
+puts "Importing CSV data from: #{filename}"
+
+csv_data = File.read(filename)
+movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+movies.each do |m|
+  puts "Movie Title: #{m['original_title']}"
+end
